8e19d1ae631b4d3738dbd8e6999b74439683101f
stephan <stephanjacobs@outlook.com>
2022-12-03 18:57:12 +1100

    implemented depth sensor and photo depth estimate in same file

--- distance_map.py
+++ distance_map.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import RPi.GPIO as GPIO
@@ -44,38 +45,47 @@ cv2.destroyAllWindows()
 # cv2.imshow("Depth Image", bwDepth)
 # cv2.waitKey(1)
 
-# 
-# GPIO.setmode(GPIO.BCM)
-# 
-# TRIG = 23
-# ECHO = 24
-# 
-# print("distance measurement in progress")
-# 
-# GPIO.setup(TRIG,GPIO.OUT)
-# GPIO.setup(ECHO,GPIO.IN)
-# 
-# GPIO.output(TRIG, False)
-# 
-# print("waiting for sensor to settle")
-# time.sleep(2)
-# 
-# GPIO.output(TRIG,True)
-# time.sleep(0.00001)
-# GPIO.output(TRIG,False)
-# 
-# while GPIO.input(ECHO)==0:
-#     pulse_start = time.time()
-# 
-# while GPIO.input(ECHO)==1:
-#     pulse_end = time.time()
-# 
-# pulse_duration = pulse_end - pulse_start
-# 
-# distance = pulse_duration * 17150
-# 
-# distance = round(distance, 2)
-# 
-# print("Distance:", distance, "cm")
-# 
-# GPIO.cleanup()
+# Run Ultrasound Distance Measure 
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23
+ECHO = 24
+
+print("distance measurement in progress")
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+
+print("waiting for sensor to settle")
+time.sleep(2)
+
+GPIO.output(TRIG,True)
+time.sleep(0.00001)
+GPIO.output(TRIG,False)
+
+while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
+
+while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start
+
+distance = pulse_duration * 17150
+
+distance = round(distance, 2)
+
+print("Distance:", distance, "cm")
+
+GPIO.cleanup()
+
+# Save file
+
+x = datetime.datetime.now()
+
+dts = x.strftime("%y%m%d_%H%M")
+
+cv2.imwrite(f'./images/depth_img_{dts}_dist{distance}.png', bwDepth*255)
+cv2.imwrite(f'./images/img_{dts}_dist{distance}.png', img)
---
+++ test_datetime.py
@@ -0,0 +1,7 @@
+import datetime
+
+x = datetime.datetime.now()
+
+dts = x.strftime("%y%m%d_%H%M")
+
+print(dts)
